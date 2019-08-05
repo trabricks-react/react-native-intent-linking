6e6d4451373a963391b0929c153c8b60a30afc97
Suhan Moon <lunyx@me.com>
2019-08-06 01:42:37 +0900

    Proccessed

--- android/src/main/java/com/trabricks/linkingwithintent/RNLinkingWithIntentModule.java
+++ android/src/main/java/com/trabricks/linkingwithintent/RNLinkingWithIntentModule.java
@@ -1,22 +1,59 @@
 
 package com.trabricks.linkingwithintent;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import java.util.List;
 
 public class RNLinkingWithIntentModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNLinkingWithIntentModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNLinkingWithIntentModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNLinkingWithIntent";
-  }
+    @Override
+    public String getName() {
+        return "RNLinkingWithIntent";
+    }
+
+    @ReactMethod
+    public void openURL(String url, Promise promise) {
+        try {
+            Intent intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            reactContext.getCurrentActivity().startActivity(intent);
+            promise.resolve(null);
+        }
+        catch(Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void canOpenURL(String url, Promise promise) {
+        try {
+            Intent intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+            PackageManager manager = reactContext.getPackageManager();
+            List<ResolveInfo> infos = manager.queryIntentActivities(intent, 0);
+
+            promise.resolve((infos.size() > 0) ? Boolean.TRUE : Boolean.FALSE);
+        }
+        catch(Exception ex) {
+            promise.reject(ex);
+        }
+
+    }
+
 }
+
+
--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-
-import { NativeModules } from 'react-native';
+import { Linking, NativeModules, Platform } from "react-native";
 
 const { RNLinkingWithIntent } = NativeModules;
 
-export default RNLinkingWithIntent;
+const openURL = (url) => {
+  if (Platform.OS !== "android" || url.indexOf("intent://") !== 0) return Linking.openURL(url);
+
+  if (RNLinkingWithIntent) {
+    return RNLinkingWithIntent.openURL(url);
+  }
+  else {
+    return new Promise((resolve, reject) => reject("not found modules"));
+  }
+};
+
+const canOpenURL = (url) => {
+  if (Platform.OS !== "android" || url.indexOf("intent://") !== 0) return Linking.canOpenURL(url);
+
+  if (RNLinkingWithIntent) {
+    return RNLinkingWithIntent.canOpenURL(url);
+  }
+  else {
+    return new Promise((resolve, reject) => reject("not found modules"));
+  }
+};
+
+
+export default {
+  ...Linking,
+  openURL,
+  canOpenURL,
+}
+
