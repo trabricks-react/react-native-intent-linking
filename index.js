cd0de41a39c3614c4f7d11fc8327343772472cef
Tagir Almasov <tccup1@gmail.com>
2019-11-25 17:38:12 +0100

    fix: intent check

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { Linking, NativeModules, Platform } from "react-native";
 const { RNLinkingWithIntent } = NativeModules;
 
 const openURL = (url) => {
-  if (Platform.OS !== "android" || url.indexOf("intent://") !== 0) return Linking.openURL(url);
+  if (Platform.OS !== "android" || url.indexOf("intent:") !== 0) return Linking.openURL(url);
 
   if (RNLinkingWithIntent) {
     return RNLinkingWithIntent.openURL(url);
@@ -14,7 +14,7 @@ const openURL = (url) => {
 };
 
 const canOpenURL = (url) => {
-  if (Platform.OS !== "android" || url.indexOf("intent://") !== 0) return Linking.canOpenURL(url);
+  if (Platform.OS !== "android" || url.indexOf("intent:") !== 0) return Linking.canOpenURL(url);
 
   if (RNLinkingWithIntent) {
     return RNLinkingWithIntent.canOpenURL(url);
